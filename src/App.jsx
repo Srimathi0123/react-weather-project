c759b17885d249d7ca9eb77bf8f0cfd379da5f6c
Srimathi0123 <Srimathi0123>
2023-09-25 16:42:04 +0530

    add.created a weather app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from 'react'
+import './app.css'
+import WeatherComponent from './Components/WeatherComponent/WeatherComponent'
+const App = () => {
   return (
-    <p>hello</p>
+    <React.Fragment>
+      <WeatherComponent/>
+    </React.Fragment>
   )
 }
 
